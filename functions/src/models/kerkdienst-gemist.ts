3dfd525015af59f459479c3132a9b9f9144c7fa3
Mart-Jan Roeleveld <67376694+EmJee1@users.noreply.github.com>
2026-01-17 12:46:48 +0100

    feat: store pastor in service

--- functions/src/models/kerkdienst-gemist.ts
+++ functions/src/models/kerkdienst-gemist.ts
@@ -1,28 +1,30 @@
 export interface IServiceEnclosure {
-	length: number
-	type: string
-	url: string
+  length: number;
+  type: string;
+  url: string;
 }
 
 export interface IServiceItunes {
-	summary: string
-	duration: string
+  summary: string;
+  duration: string;
+  author?: string;
 }
 
 export interface IKDGService {
-	title: string
-	link: string
-	pubDate: string
-	enclosure: IServiceEnclosure
-	content: string
-	contentSnippet: string
-	guid: string
-	isoDate: string
-	itunes: IServiceItunes
+  title: string;
+  link: string;
+  pubDate: string;
+  enclosure: IServiceEnclosure;
+  content: string;
+  contentSnippet: string;
+  guid: string;
+  isoDate: string;
+  itunes: IServiceItunes;
 }
 
 export interface IService {
-	title: string
-	file: string
-	createdAt: Date
+  title: string;
+  file: string;
+  pastor?: string;
+  createdAt: Date;
 }
