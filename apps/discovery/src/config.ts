a69c29d6940899aa42ae8abbca7ac270e5e07ce3
Mart-Jan Roeleveld <67376694+EmJee1@users.noreply.github.com>
2026-08-25 07:49:18 +0200

    feat: fetch feed config in discovery

--- apps/discovery/src/config.ts
+++ apps/discovery/src/config.ts
@@ -1,16 +1,35 @@
+import { err, ok } from 'neverthrow';
+
 import { valueForEnvironment } from '#environment';
+
+function stringConfigOrFail(variable: string) {
+  const value = process.env[variable];
+  return value ? ok(value) : err({ kind: 'missingEnvironmentVariable', variable });
+}
 
 export const config = valueForEnvironment({
   local: {
     port: 8080,
     host: 'localhost',
+    kdgFeed: {
+      id: stringConfigOrFail('KDG_FEED_ID'),
+      accessKey: stringConfigOrFail('KDG_ACCESS_KEY'),
+    },
   },
   staging: {
     port: Number(process.env.PORT),
     host: '0.0.0.0',
+    kdgFeed: {
+      id: stringConfigOrFail('KDG_FEED_ID'),
+      accessKey: stringConfigOrFail('KDG_ACCESS_KEY'),
+    },
   },
   production: {
     port: Number(process.env.PORT),
     host: '0.0.0.0',
+    kdgFeed: {
+      id: stringConfigOrFail('KDG_FEED_ID'),
+      accessKey: stringConfigOrFail('KDG_ACCESS_KEY'),
+    },
   },
 });
--- apps/discovery/src/index.ts
+++ apps/discovery/src/index.ts
@@ -1,8 +1,11 @@
 import http from 'node:http';
 
+import { fetchKdgRssFeed } from '@kdg/feed';
+import { Result } from 'neverthrow';
+
 import { config } from '#config';
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   if (req.url !== '/discover' || req.method !== 'POST') {
     res.writeHead(404, { 'Content-Type': 'application/json' }).end(
       JSON.stringify({
@@ -15,6 +18,31 @@ const server = http.createServer((req, res) => {
   }
 
   console.log('Received discovery request');
+
+  const feedConfig = Result.combine([config.kdgFeed.id, config.kdgFeed.accessKey]);
+  if (!feedConfig.isOk()) {
+    res.writeHead(500, { 'Content-Type': 'application/json' }).end(
+      JSON.stringify({
+        error: {
+          message: 'Internal server error',
+        },
+      }),
+    );
+    return;
+  }
+
+  const [kdgFeedId, kdgFeedPlaylistId] = feedConfig.value;
+  const feed = await fetchKdgRssFeed(kdgFeedId, kdgFeedPlaylistId, 2);
+  if (!feed.isOk()) {
+    res.writeHead(502, { 'Content-Type': 'application/json' }).end(
+      JSON.stringify({
+        error: {
+          message: 'Fetching RSS feed from Kerkdienstgemist failed',
+        },
+      }),
+    );
+    return;
+  }
 
   res.writeHead(200, { 'Content-Type': 'application/json' }).end(JSON.stringify({ ok: true }));
 });
